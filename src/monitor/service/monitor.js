48af96202ab0c4cbd00f618a6f658ee511ec8eb3
keller35 <524495217@qq.com>
2015-11-12 15:35:16 +0800

    monitorPosters、monitorCategories接口

--- src/monitor/service/monitor.js
+++ src/monitor/service/monitor.js
@@ -5,9 +5,14 @@ var info = require("./info.json");
 exports.start = start;
 
 var TIME_INTERVAL_ACTIVITY = 1000 * 60 * 60 * 1;
+var TIME_INTERVAL_POSTERS = 1000 * 60 * 60 * 1;
+var TIME_INTERVAL_CATEGORIES = 1000 * 60 * 60 * 1;
+
 //启动监控任务
 function start(){
     setInterval(monitorActivity, TIME_INTERVAL_ACTIVITY);
+    setInterval(monitorPosters, TIME_INTERVAL_POSTERS);
+    setInterval(monitorCategories, TIME_INTERVAL_CATEGORIES);
 }
 
 //当期活动详情接口监控
@@ -87,5 +92,53 @@ function monitorActivity(){
         if(flag){
             services.sendMsg(param);
         }
+    });
+}
+
+//posters接口监控
+function monitorPosters(){
+
+    var api = "/posters";
+    var url = info.host + api;
+    var option = {
+        method: "GET",
+        uri: url,
+        json:true
+    };
+    request(option, function(error, response, body){
+        if(error){
+            return logger.error(error);
+        }
+        //接口正常
+        if(body && body.result && body.result.posters && body.result.posters.length > 0){
+            return ;
+        }
+        //接口异常，发送短信
+        param = "接口" + api + "," + "数据为空";
+        services.sendMsg(param);
+    });
+}
+
+//categories接口监控
+function monitorCategories(){
+
+    var api = "/categories";
+    var url = info.host + api;
+    var option = {
+        method: "GET",
+        uri: url,
+        json:true
+    };
+    request(option, function(error, response, body){
+        if(error){
+            return console.log(error);
+        }
+        //接口正常
+        if(body && body.result && body.result.categories && body.result.categories.length > 0){
+            return ;
+        }
+        //接口异常，发送短信
+        param = "接口" + api + "," + "数据为空";
+        services.sendMsg(param);
     });
 }
